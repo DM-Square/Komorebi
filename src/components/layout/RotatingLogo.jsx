5fd0647ee739eafc38fd5783d93d2d4a953550d2
Maurizio <mauriziodm.93@gmail.com>
2026-04-20 12:33:41 -0700

    feat: add AnimatePresence to RotatingLogo for improved animation handling

--- src/components/layout/RotatingLogo.jsx
+++ src/components/layout/RotatingLogo.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 //eslint-disable-next-line no-unused-vars
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
 import { ASSETS } from "../../constants/theme";
 
 const RotatingLogo = () => {
@@ -15,16 +15,18 @@ const RotatingLogo = () => {
   }, [logos.length]);
 
   return (
-    <motion.img
-      key={logoIndex}
-      src={logos[logoIndex]}
-      alt="Komorebi Logo"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 3.5 }}
-      className="flex px-3 object-contain w-30 h-10 md:w-40"
-    />
+    <AnimatePresence mode="wait">
+      <motion.img
+        key={logoIndex}
+        src={logos[logoIndex]}
+        alt="Komorebi Logo"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1.5 }}
+        className="flex px-3 object-contain w-30 h-10 md:w-40"
+      />
+    </AnimatePresence>
   );
 };
 
