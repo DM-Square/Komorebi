ff6db85c29d7e49aee49db089895e84754cef619
Maurizio <mauriziodm.93@gmail.com>
2026-04-20 12:34:27 -0700

    refactor: wrap Meditate component in TimerProvider for context management

--- src/pages/Meditate.jsx
+++ src/pages/Meditate.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import Timer from "../components/Timer/Timer";
 import Footer from "../components/layout/Footer";
 import WaveBackground from "../components/ui/WaveBackground";
+import { TimerProvider } from "../context/TimerProvider";
 
 const Meditate = () => {
   return (
-    <>
+    <TimerProvider>
       <div className="flex h-fit flex-col items-center justify-center text-center bg-mist-800 px-7 relative overflow-hidden">
         <WaveBackground />
         <div className="relative z-10">
@@ -13,7 +14,7 @@ const Meditate = () => {
           <Footer />
         </div>
       </div>
-    </>
+    </TimerProvider>
   );
 };
 
